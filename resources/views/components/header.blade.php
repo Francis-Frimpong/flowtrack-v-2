0b4d3b59fa3b9af42c9b820cf93e0e0734ca9632
Francis <brakay1136@gmail.com>
2026-09-07 06:45:32 -0800

    fixed styling bug for header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header  class=" w-full p-4 py-8 bg-slate-900 text-white py-[18px] sticky top-0">
+<header  class=" w-full p-4 py-8 bg-slate-900 text-white py-[18px] sticky top-0 z-50">
     <div class="container mx-auto flex justify-between items-center px-8">
         <h1 class="text-[2rem] md:text-5xl text-3xl font-semibold">FlowTrack</h1>
         <nav class="hidden md:flex items- space-x-4 flex gap-5">
