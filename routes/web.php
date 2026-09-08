10d1028533d4cdd538419e3d84aae531500fc24a
Francis <brakay1136@gmail.com>
2026-09-08 06:28:29 -0800

    login route and controller created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingPageController::class, 'index']);
+Route::get('/login', [LoginController::class, 'login']);
