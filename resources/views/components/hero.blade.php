00031019f7087f59e29b62c23c53e59165f04792
Francis <brakay1136@gmail.com>
2026-09-09 08:55:19 -0800

    made hero component responsive

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,11 +1,16 @@
 <section class="flex items-center py-24 text-white bg-gradient-to-br from-slate-900 to-blue-800">
-    <div class="container max-auto text-center z-10">
-        <h1 class="text-5xl font-bold mb-5">Focus Better. Achieve More.</h1>
-        <p class="max-w-[700px] mx-auto mb-[30px] text-lg">
-        FlowTrack helps students, developers, freelancers, and professionals track tasks,
-        manage focus sessions, and build productive habits every day.
+    <div class="w-[92%] max-w-[1100px] mx-auto text-center z-10">
+
+        <h1 class="text-4xl sm:text-5xl lg:text-6xl font-bold mb-5">
+            Focus Better. Achieve More.
+        </h1>
+
+        <p class="max-w-[700px] mx-auto mb-[30px] text-base sm:text-lg">
+            FlowTrack helps students, developers, freelancers, and professionals track tasks,
+            manage focus sessions, and build productive habits every day.
         </p>
-       <x-button-link>Start Free</x-button-link>
+
+        <x-button-link>Start Free</x-button-link>
+
     </div>
-    
 </section>
