1378a6379ffa715adb425a68eb7310bfc111e0e2
Francis <brakay1136@gmail.com>
2026-09-24 10:37:12 -0800

    error message for from field created

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,3 +1,6 @@
 @props(['labelClass' => null, 'inputClass' => null, 'type' => null, 'name' => null, 'placeholder' => null, 'for' => null, 'value' => null])
 <label class="{{ $labelClass }}" for="{{ $for }}">{{$slot}}</label>
 <input type="{{ $type }}" placeholder="{{ $placeholder }}" class="{{ $inputClass }}" name="{{ $name }}" value="{{ $value }}">
+@error($name)
+  <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+@enderror
